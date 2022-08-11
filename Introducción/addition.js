9e149d3499782a6dad3658342130c3b6c64f4597
hozlucas28 <hozlucas28@gmail.com>
2022-08-11 00:22:19 -0300

    Nombres de archivos reescritos al ingles: local --> remoto

--- Introducción/addition.js
+++ Introducción/addition.js
@@ -1,6 +1,6 @@
 // cSpell:disable
 /* -------------------------------- Variables ------------------------------- */
-let sum = 0;
+let addition = 0;
 let firstNumber = prompt('Ingrese un número'); //Obtiene el valor ingresado en la página, mediante una alerta.
 let secondNumber = prompt('Ingrese un número');
 
@@ -10,8 +10,8 @@ secondNumber = Number(secondNumber);
 
 
 /* ------------------------------- Operaciones ------------------------------ */
-sum = firstNumber + secondNumber;
+addition = firstNumber + secondNumber;
 
 
 /* --------------------------------- Salidas -------------------------------- */
-document.write('La suma es: ', sum);
+document.write('La suma es: ', addition);
