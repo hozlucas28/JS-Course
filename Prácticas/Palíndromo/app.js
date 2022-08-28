6c9a24d99a35827acad6d8fd60f0dd2934b3058a
hozlucas28 <hozlucas28@gmail.com>
2022-08-28 17:37:45 -0300

    Refactor of several files

---
+++ Prácticas/Palíndromo/app.js
@@ -0,0 +1,21 @@
+
+/* -------------------------- Variables y Entradas -------------------------- */
+let word = prompt('Ingrese una palabra:');
+
+
+
+/* -------------------------------- Funciones ------------------------------- */
+
+const isPalindrome = (word) => {
+    let invertedWord;
+
+    word = word.toLowerCase().replace(' ', '');
+    invertedWord = word.split('').reverse().join('');
+    return (word === invertedWord);
+};
+
+
+
+/* --------------------------------- Salidas -------------------------------- */
+
+isPalindrome(word)? document.write('La palabra ', word, ' es palíndroma.') : document.write('La palabra ', word, ' no es palíndroma.');
