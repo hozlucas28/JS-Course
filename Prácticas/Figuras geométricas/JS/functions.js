407a2975dd0335bf44d0b390dc1d464f67c07400
hozlucas28 <hozlucas28@gmail.com>
2022-09-24 21:41:02 -0300

    Update 'Figuras geométricas' practice

--- Prácticas/Figuras geométricas/JS/functions.js
+++ Prácticas/Figuras geométricas/JS/functions.js
@@ -1,78 +1,92 @@
 
-/* -------------------------------- Cuadrado -------------------------------- */
+(() => {
+    'use strict'
 
-function calculateSquareArea() {
-    let sideValue = Number(document.getElementById('square-side-input').value),
-        result = document.getElementById('answer-result');
-    
-    sideValue *= 2;
-    result.innerHTML = `El área del cuadrado es: ${String(sideValue)} cm<sup>2</sup>`;
-};
-
-
-function calculateSquarePerimeter() {
-    let sideValue = Number(document.getElementById('square-side-input').value),
-        result = document.getElementById('answer-result');
-    
-    sideValue *= 4;
-    result.innerHTML = `El perímetro del cuadrado es: ${String(sideValue)} cm`;
-};
+    const calculateSquarePerimeter = document.getElementById('square-perimeter-button'),
+          calculateSquareArea = document.getElementById('square-area-button'),
+          calculateTrianglePerimeter = document.getElementById('triangle-perimeter-button'),
+          calculateTriangleArea = document.getElementById('triangle-area-button'),
+          calculateCircleDiameter = document.getElementById('circle-diameter-button'),
+          calculateCirclePerimeter = document.getElementById('circle-perimeter-button'),
+          calculateCircleArea = document.getElementById('circle-area-button');
 
 
+          
+    /* -------------------------------- Cuadrado -------------------------------- */
 
-/* -------------------------------- Triangulo ------------------------------- */
-
-function calculateTriangleArea() {
-    let operation;
-    let baseValue = Number(document.getElementById('triangle-base-input').value),
-        heightValue = Number(document.getElementById('triangle-height-input').value),
-        result = document.getElementById('answer-result');
-
-    operation = (baseValue * heightValue) / 2;
-    result.innerHTML = `El área del triangulo es: ${String(operation)} cm<sup>2</sup>`;
-};
-
-
-function calculateTrianglePerimeter() {
-    let operation;
-    let firstSideValue = Number(document.getElementById('triangle-first-side-input').value),
-        secondSideValue = Number(document.getElementById('triangle-second-side-input').value),
-        thirdSideValue = Number(document.getElementById('triangle-third-side-input').value),
-        result = document.getElementById('answer-result');
+    calculateSquareArea.addEventListener('click', () => {
+        let sideValue = Number(document.getElementById('square-side-input').value),
+            result = document.getElementById('answer-result');
         
-    operation = firstSideValue + secondSideValue + thirdSideValue;
-    result.innerHTML = `El perímetro del triangulo es: ${String(operation)} cm`;
-};
+        sideValue *= 2;
+        result.innerHTML = `El área del cuadrado es: ${String(sideValue)} cm<sup>2</sup>`;
+    });
+
+
+    calculateSquarePerimeter.addEventListener('click', () => {
+        let sideValue = Number(document.getElementById('square-side-input').value),
+            result = document.getElementById('answer-result');
+        
+        sideValue *= 4;
+        result.innerHTML = `El perímetro del cuadrado es: ${String(sideValue)} cm`;
+    });
 
 
 
-/* --------------------------------- Circulo -------------------------------- */
+    /* -------------------------------- Triangulo ------------------------------- */
 
-function calculateDiameterArea() {
-    let operation;
-    let radiusValue = Number(document.getElementById('circle-radius-input').value),
-        result = document.getElementById('answer-result');
+    calculateTriangleArea.addEventListener('click', () => {
+        let operation;
+        let baseValue = Number(document.getElementById('triangle-base-input').value),
+            heightValue = Number(document.getElementById('triangle-height-input').value),
+            result = document.getElementById('answer-result');
 
-    operation = radiusValue * 2;
-    result.innerHTML = `El diametro del circulo es: ${String(operation)} cm`;
-};
-
-
-function calculateCircleArea() {
-    let operation;
-    let radiusValue = Number(document.getElementById('circle-radius-input').value),
-        result = document.getElementById('answer-result');
-
-    operation = Math.pow(radiusValue, 2);
-    result.innerHTML = `El área del circulo es: ${String(operation)}π cm<sup>2</sup>`;
-};
+        operation = (baseValue * heightValue) / 2;
+        result.innerHTML = `El área del triangulo es: ${String(operation)} cm<sup>2</sup>`;
+    });
 
 
-function calculateCirclePerimeter() {
-    let operation;
-    let radiusValue = Number(document.getElementById('circle-radius-input').value),
-        result = document.getElementById('answer-result');
+    calculateTrianglePerimeter.addEventListener('click', () => {
+        let operation;
+        let firstSideValue = Number(document.getElementById('triangle-first-side-input').value),
+            secondSideValue = Number(document.getElementById('triangle-second-side-input').value),
+            thirdSideValue = Number(document.getElementById('triangle-third-side-input').value),
+            result = document.getElementById('answer-result');
+            
+        operation = firstSideValue + secondSideValue + thirdSideValue;
+        result.innerHTML = `El perímetro del triangulo es: ${String(operation)} cm`;
+    });
 
-    operation = radiusValue * 2;
-    result.innerHTML = `El perímetro del circulo es: ${String(operation)}π cm`;
-};
+
+
+    /* --------------------------------- Circulo -------------------------------- */
+
+    calculateCircleDiameter.addEventListener('click', () => {
+        let operation;
+        let radiusValue = Number(document.getElementById('circle-radius-input').value),
+            result = document.getElementById('answer-result');
+
+        operation = radiusValue * 2;
+        result.innerHTML = `El diametro del circulo es: ${String(operation)} cm`;
+    });
+
+
+    calculateCircleArea.addEventListener('click', () => {
+        let operation;
+        let radiusValue = Number(document.getElementById('circle-radius-input').value),
+            result = document.getElementById('answer-result');
+
+        operation = Math.pow(radiusValue, 2);
+        result.innerHTML = `El área del circulo es: ${String(operation)}π cm<sup>2</sup>`;
+    });
+
+
+    calculateCirclePerimeter.addEventListener('click', () => {
+        let operation;
+        let radiusValue = Number(document.getElementById('circle-radius-input').value),
+            result = document.getElementById('answer-result');
+
+        operation = radiusValue * 2;
+        result.innerHTML = `El perímetro del circulo es: ${String(operation)}π cm`;
+    });
+}) ();
